4031c66ab194a293ba4a3179b836ff9baa730a77
Ed Zempel <edward.zempel@gmail.com>
2018-12-10 00:10:16 -0600

    First commit added play.py to sketch out connect to movie API and allow the user to enter the search query

---
+++ play.py
@@ -0,0 +1,34 @@
+import requests
+import collections
+
+MovieResult = collections.namedtuple(
+    'MovieResult',
+    "imdb_code,title,duration,director,year,rating,imdb_score,keywords,genres"
+)
+
+search = input("What movie do you want to search for? ")
+url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
+
+resp = requests.get(url)
+resp.raise_for_status()
+
+movie_data = resp.json()
+movie_list = movie_data.get('hits')
+
+movies = []
+for md in movie_list:
+    m = MovieResult(
+        imdb_code=md.get('imdb_code'),
+        title=md.get('title'),
+        duration=md.get('duration'),
+        director=md.get('director'),
+        year=md.get('year', 0.0),
+        rating=md.get('rating', 0.0),
+        imdb_score=md.get('imdb_score', 0.0),
+        keywords=md.get('keywords'),
+        genres=md.get('genres'),
+    )
+    movies.append(m)
+
+for m in movies:
+    print("{} -- {}".format(m.year, m.title))
